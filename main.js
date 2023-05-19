5bc3ae5cb15b5f3e52b494316a4b8c837f75bd74
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-05-19 15:42:39 -0500

    feat: :hammer: update
    updated js for bug fix because i am a stoopid

--- main.js
+++ main.js
@@ -1,34 +1,27 @@
-let all = document.getElementById('all')
-all.addEventListener('click',crearCarta());
-
+document.addEventListener("DOMContentLoaded", function(){
+  crearCarta();
+});
 
 async function crearCarta(){
-    fetch('https://overfast-api.tekrop.fr/heroes?locale=en-us')
-    .then(response => response.json())
-    .then(data=>{
-        const nombre = data.name;
-        const role = data.role;
-        const portrait = data.portrait;
+  try {
+    const response = await fetch('./heroes.json');
+    const data = await response.json();
 
-        const card = document.createElement('div');
-        card.classList.add('carta');
+    data.forEach(objeto =>{
+      const nombre = objeto.name;
+      const portrait = objeto.portrait;
+      const role = objeto.role;
 
-        const imagen = document.createElement('img');
-        imagen.src = portrait;
-        card.appendChild(imagen);
+      let card = document.createElement('div');
+      card.classList.add('cartita')
+      card.innerHTML = `
+      
+      `
 
-        const name = document.createElement('p');
-        name.textContent = `${nombre}`;
-        card.appendChild(name);
-
-        const rol = document.createElement('p');
-        rol.textContent = `${role}`;
-        card.appendChild(rol);
-
-        const contenedorcartas = document.getElementById('contenedorCartas');
-        contenedorcartas.appendChild(card);
     })
-    .catch(error =>{
-        console.log('xd se me buggeo la api')
-    })
-}
+
+  }
+  catch (error){
+    console.log('EL API ME FUNO')
+  }
+};
